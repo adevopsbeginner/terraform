326c2ac813a950f9e6c2c690faacefe2e0fcdf3a
adevopsbeginner <houxizhu@gmail.com>
2025-05-15 12:27:09 +1000

    new p14s local keypair

--- keypair/main.tf
+++ keypair/main.tf
@@ -3,11 +3,13 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-resource "tls_private_key" "ed25519" {
-  algorithm = "ED25519"
-}
+# resource "tls_private_key" "ed25519" {
+#   algorithm = "ED25519"
+# }
 
 resource "aws_key_pair" "t_abeginner" {
   key_name   = "t_abeginner_ed25519"
-  public_key = tls_private_key.ed25519.public_key_openssh
+  #public_key = tls_private_key.ed25519.public_key_openssh
+  public_key = file("id_ed25519.pub")
 }
+
